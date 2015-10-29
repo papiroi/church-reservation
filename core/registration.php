ad5c217ef0c053190139365ea2b07884054707dc
Michael Adam <madamt0001@gmail.com>
2015-10-29 08:35:37 +0800

    update as of 102915 8:35am

--- core/registration.php
+++ core/registration.php
@@ -13,7 +13,97 @@
 
 class Registration {
 
-
+	public $conn;
+	public $firstname;
+	public $lastname;
+	public $mobile;
+	public $email;
+	public $address;
+	public $bday;
+	public $username;
+	public $password;
+	
+	
+	public function __construct($c, $fn, $ln, $mn, $e, $add, $bday, $u, $p) {
+	
+		$this->conn = $c;
+		$this->firstname = $fn;
+		$this->lastname = $ln;
+		$this->mobile = $mn;
+		$this->email = $e;
+		$this->address = $add;
+		$this->bday = $bday;
+		$this->username = $u;
+		$this->password = $p;
+		
+	}
+	
+	public function check_user($user) {
+	
+		$check_query = "SELECT * FROM users WHERE username = '$user'";
+		
+		$check_query_result = $this->conn->query($check_query);
+		
+		if($check_query_result->num_rows > 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public function register() {
+	
+		if($this->check_user($this->username) == 'true') {
+			echo "User Already Exists!";
+		}
+		else {
+			$reg_query = "INSERT INTO users (username, password, firstname, lastname, mobile, email, address, bday, status, dateReg)
+				VALUES(
+					'$this->username',
+					'$this->password',
+					'$this->firstname',
+					'$this->lastname',
+					'$this->mobile',
+					'$this->email',
+					'$this->address',
+					'$this->bday',
+					'Active',
+					'NOW()'
+				)";
+	
+			$reg_query_result = $this->conn->query($reg_query);
+		
+			if($reg_query_result) {
+				// Successfull Creation of User
+				echo "Successfully Created User!";
+			}
+			else {
+				echo "Error in Registration! Try Again Later.";
+			}
+		}
+	}
+	
+	// This method Generates 8 Random Characters to server as a recovery character in case 
+	// That the user will forget the password or username
+	public function recoveryStringGen() {
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$length = 8;
+		$charactersLength = strlen($characters);
+		$randomString = '';
+		for ($i = 0; $i < $length; $i++) {
+			$randomString .= $characters[rand(0, $charactersLength - 1)];
+		}
+		return $randomString;
+	}
+	
+	// Activation Code Generator
+	// 
+	// 
+	public function activationGen() {
+	
+	
+	}
 
 }
 	
--- register.php
+++ register.php
@@ -48,11 +48,34 @@
 		<div class="row">
 		<div class="col-md-5">
 		<div class="center-div">
-		
+		<span class="error_message">
 <!-- Start of PHP Code in Registration -->
 <?php
 	if(isset($_POST['username']) && !empty($_POST['username'])) {
+	
+		// Include Database Connection String
+		require_once "includes/connect.php";
+	
+		// Include Validation/Sanitation Class
+		require_once "core/validation.php";
+		
 		// Set Variable Holders and other Operations
+		
+		$firstname = stripslashes($_POST['firstname']);
+		$lastname = stripslashes($_POST['lastname']);
+		$mobile = stripslashes($_POST['mobile']);
+		$email = stripslashes($_POST['email']);
+		$address = stripslashes($_POST['address']);
+		$bday = stripslashes($_POST['bday']);
+		$username = stripslashes($_POST['username']);
+		$password = sha1(stripslashes($_POST['password']));
+		
+		// Include Registration Class
+		require_once "core/registration.php";
+		
+		$reg = new Registration($conn, $firstname, $lastname, $mobile, $email, $address, $bday, $username, $password);
+	
+		echo $reg -> register();
 	}
 	else {
 		// Nothing to do here just run the page as is
@@ -60,12 +83,14 @@
 
 ?>
 <!-- End of PHP Code in Registration -->
+			</span>
 			<h3 class="white-text">Registration Form</h3>
 			<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
 				
 				<div id="firstnameclassdiv" class="form-group has-feedback">
 				<label for="firstname">First Name:</label>
-				<input type="text" name="firstname" id="firstname" class="form-control input-width" placeholder="First Name" title="First Name Field is Required!" required/>
+				<input type="text" name="firstname" id="firstname" class="form-control input-width" 
+					placeholder="First Name" title="First Name Field is Required!" required/>
 				<span id="firstnameclassspan" class="glyphicon form-control-feedback"></span>
 				</div>
 				
@@ -73,7 +98,8 @@
 				
 				<div id="lastnameclassdiv" class="form-group has-feedback">
 				<label for="lastname">Lastname:</label>
-				<input type="text" name="lastname" id="lastname" class="form-control input-width" placeholder="Last Name" required/>
+				<input type="text" name="lastname" id="lastname" class="form-control input-width" 
+					placeholder="Last Name" title="Lastname is Required!" required/>
 				<span id="lastnameclassspan" class="glyphicon form-control-feedback"></span>
 				</div>
 				
@@ -81,7 +107,8 @@
 				
 				<div id="mobileclassdiv" class="form-group has-feedback">
 				<label for="mobile">Mobile Number:</label><i> 11 Digit Number</i>
-				<input type="number" name="mobile" id="mobile" class="form-control input-width" placeholder="Mobile Number" required/>
+				<input type="number" name="mobile" id="mobile" class="form-control input-width" 
+					placeholder="Mobile Number" title="Mobile Number is Required!" required/>
 				<span id="mobileclassspan" class="glyphicon form-control-feedback"></span>
 				</div>
 				
@@ -89,7 +116,8 @@
 				
 				<div id="emailclassdiv" class="form-group has-feedback">
 				<label for="email">Email:</label>
-				<input type="email" name="email" id="email" class="form-control input-width" placeholder="Email Address" required/>
+				<input type="email" name="email" id="email" class="form-control input-width" 
+					placeholder="Email Address" title="Email Address is Required!" required/>
 				<span id="emailclassspan" class="glyphicon form-control-feedback"></span>
 				</div>
 				
@@ -97,7 +125,8 @@
 				
 				<div id="addressclassdiv" class="form-group has-feedback">
 				<label for="address">Address:</label>
-				<input type="text" name="address" id="address" class="form-control input-width" placeholder="Address" required/>
+				<input type="text" name="address" id="address" class="form-control input-width" 
+					placeholder="Address" title="Address is Required" required/>
 				<span id="addressclassspan" class="glyphicon form-control-feedback"></span>
 				</div>
 				
@@ -114,7 +143,7 @@
 				<div id="usernameclassdiv" class="form-group has-feedback">
 				<label for="username">Username:</label>
 				<input type="text" name="username" id="username" class="form-control input-width" 
-					placeholder="Username" pattern=".{5,32}" required 
+					placeholder="Username" pattern=".{5,32}" required maxlength="32" min="5"
 					title="It must be a minimum of 5 characters and a maximum of 32 characters!"/>
 				<span id="usernameclassspan" class="glyphicon form-control-feedback"></span>
 				</div>
@@ -123,7 +152,9 @@
 				
 				<div id="passwordclassdiv" class="form-group has-feedback">
 				<label for="password">Password:</label>
-				<input type="password" name="passwrod" id="password" class="form-control input-width" placeholder="Password" required/>
+				<input type="password" name="password" id="password" class="form-control input-width" 
+					placeholder="Password" pattern=".{8,32}" required maxlength="32" min="8"
+					title="Password must not be a minimum of 8 characters and a maximum of 32!"/>
 				<span id="passwordclassspan" class="glyphicon form-control-feedback"></span>
 				</div>
 				
@@ -131,7 +162,9 @@
 				
 				<div id="password2classdiv" class="form-group has-feedback">
 				<label for="password2">Re-Enter Password:</label>
-				<input type="password" name="password2" id="password2" class="form-control input-width" placeholder="Re-Type Password" required/>
+				<input type="password" name="password2" id="password2" class="form-control input-width" 
+					placeholder="Re-Type Password" pattern=".{8,32}" required maxlength="32"
+					title="Must be the same as the first password you enter!"/>
 				<span id="password2classspan" class="glyphicon form-control-feedback"></span>
 				</div>
 				
@@ -153,7 +186,7 @@
 <script>
 $(document).ready(function() {
 	// Validation for Firstname
-	$("#firstname").focusout(function() {
+	$("#firstname").keyup(function() {
 		if($("#firstname").val().length == "") {
 			$( "#firstnameclassdiv" ).removeClass( "has-success" ).addClass( "has-error" );
 			$( "#firstnameclassspan" ).removeClass( "glyphicon-ok" ).addClass( "glyphicon-remove" );
@@ -165,7 +198,7 @@ $(document).ready(function() {
 	});
 	
 	// Validation for Lastname
-	$("#lastname").focusout(function() {
+	$("#lastname").keyup(function() {
 		if($("#lastname").val().length == "") {
 			$( "#lastnameclassdiv" ).removeClass( "has-success" ).addClass( "has-error" );
 			$( "#lastnameclassspan" ).removeClass( "glyphicon-ok" ).addClass( "glyphicon-remove" );
@@ -177,7 +210,7 @@ $(document).ready(function() {
 	});
 	
 	// Validation for Mobile Number
-	$("#mobile").focusout(function() {
+	$("#mobile").keyup(function() {
 		if($("#mobile").val().length == "" || $("#mobile").val().length < 11) {
 			$( "#mobileclassdiv" ).removeClass( "has-success" ).addClass( "has-error" );
 			$( "#mobileclassspan" ).removeClass( "glyphicon-ok" ).addClass( "glyphicon-remove" );
@@ -190,7 +223,7 @@ $(document).ready(function() {
 	
 	
 	// Email Validation
-	$("#email").focusout(function() {
+	$("#email").keyup(function() {
 		
 		var email = $("#email").val();
 		
@@ -210,9 +243,32 @@ $(document).ready(function() {
 		}
 	});
 	
+	// Validation for Address
+	$("#address").keyup(function() {
+		if($("#address").val().length == "" || $("#address").val().length < 4 ) {
+			$( "#addressclassdiv" ).removeClass( "has-success" ).addClass( "has-error" );
+			$( "#addressclassspan" ).removeClass( "glyphicon-ok" ).addClass( "glyphicon-remove" );
+		}
+		else {
+			$( "#addressclassdiv" ).removeClass( "has-error" ).addClass( "has-success" );
+			$( "#addressclassspan" ).removeClass( "glyphicon-remove" ).addClass( "glyphicon-ok" );
+		}
+	});
+	
+	// Validation for Birthday
+	$("#bday").keyup(function() {
+		if($("#bday").val().length == "") {
+			$( "#bdayclassdiv" ).removeClass( "has-success" ).addClass( "has-error" );
+			$( "#bdayclassspan" ).removeClass( "glyphicon-ok" ).addClass( "glyphicon-remove" );
+		}
+		else {
+			$( "#bdayclassdiv" ).removeClass( "has-error" ).addClass( "has-success" );
+			$( "#bdayclassspan" ).removeClass( "glyphicon-remove" ).addClass( "glyphicon-ok" );
+		}
+	});
 	
 	// Validation for Username
-	$("#username").focusout(function() {
+	$("#username").keyup(function() {
 		if($("#username").val().length == "" || $("#username").val().length < 5 || $("#username").val().length > 32) {
 			$( "#usernameclassdiv" ).removeClass( "has-success" ).addClass( "has-error" );
 			$( "#usernameclassspan" ).removeClass( "glyphicon-ok" ).addClass( "glyphicon-remove" );
@@ -220,6 +276,36 @@ $(document).ready(function() {
 		else {
 			$( "#usernameclassdiv" ).removeClass( "has-error" ).addClass( "has-success" );
 			$( "#usernameclassspan" ).removeClass( "glyphicon-remove" ).addClass( "glyphicon-ok" );
+		}
+	});
+	
+	// Validation for Password
+	$("#password").keyup(function() {
+		if($("#password").val().length == "" || $("#password").val().length < 8) {
+			$( "#passwordclassdiv" ).removeClass( "has-success" ).addClass( "has-error" );
+			$( "#passwordclassspan" ).removeClass( "glyphicon-ok" ).addClass( "glyphicon-remove" );
+		}
+		else {
+			$( "#passwordclassdiv" ).removeClass( "has-error" ).addClass( "has-success" );
+			$( "#passwordclassspan" ).removeClass( "glyphicon-remove" ).addClass( "glyphicon-ok" );
+		}
+	});
+	
+	// Validation for Password2
+	$("#password2").keyup(function() {
+		if($("#password2").val().length == "" || $("#password2").val().length < 8) {
+			$( "#password2classdiv" ).removeClass( "has-success" ).addClass( "has-error" );
+			$( "#password2classspan" ).removeClass( "glyphicon-ok" ).addClass( "glyphicon-remove" );
+		}
+		else {
+			if($("#password2").val() == $("#password").val()) {
+				$( "#password2classdiv" ).removeClass( "has-error" ).addClass( "has-success" );
+				$( "#password2classspan" ).removeClass( "glyphicon-remove" ).addClass( "glyphicon-ok" );
+			}
+			else {
+				$( "#password2classdiv" ).removeClass( "has-success" ).addClass( "has-error" );
+				$( "#password2classspan" ).removeClass( "glyphicon-ok" ).addClass( "glyphicon-remove" );
+			}
 		}
 	});
 
