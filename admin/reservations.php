7e97d0475fbba4a81cf8890d32c5ae6caf2ba328
Michael Adam <madamt0001@gmail.com>
2016-01-20 20:49:39 +0800

    fixed time display

--- admin/reservations.php
+++ admin/reservations.php
@@ -94,6 +94,72 @@
 			
 <?php
 
+/*
+* Function to convert Number values of Time to Time Slots
+* Calling this function to convert certain values to time value
+* num_to_time()
+*/
+	
+	function num_to_time($val) {
+		
+		if($val == 1) {
+			$val = "8:00am";
+		}
+		else if ($val == 2) {
+			$val = "8:30am";
+		}
+		else if ($val == 3) {
+			$val = "9:00am";
+		}
+		else if ($val == 4) {
+			$val = "9:30am";
+		}
+		else if ($val == 5) {
+			$val = "10:00am";
+		}
+		else if ($val == 6) {
+			$val = "10:30am";
+		}
+		else if ($val ==7) {
+			$val = "11:00am";
+		}
+		else if ($val == 8) {
+			$val = "11:30am";
+		}
+		else if ($val == 9) {
+			$val = "12:00pm";
+		}
+		else if ($val == 10) {
+			$val = "1:00pm";
+		}
+		else if ($val == 11) {
+			$val = "1:30pm";
+		}
+		else if ($val == 12) {
+			$val = "2:00pm";
+		}
+		else if ($val == 13) {
+			$val = "2:30pm";
+		}
+		else if ($val == 14) {
+			$val = "3:00pm";
+		}
+		else if ($val == 15) {
+			$val = "3:30pm";
+		}
+		else if ($val == 16) {
+			$val = "4:00pm";
+		}
+		else if ($val == 17) {
+			$val = "4:30pm";
+		}
+		else if ($val == 18) {
+			$val = "5:00pm";
+		}
+		
+		return $val;
+		
+	}
 	$select_all_reserv = "SELECT * FROM reservation";
 	$select_query_result = $conn->query($select_all_reserv);
 	
@@ -115,7 +181,7 @@
 			echo "<td>" . $row['reserv_num'] . "</td>";
 			echo "<td>" . $row['event_type'] . "</td>";
 			echo "<td>" . $row['reserv_date'] . "</td>";
-			echo "<td>" . $row['reserv_time'] . "</td>";
+			echo "<td>" . num_to_time($row['reserv_time']) . "</td>";
 			echo "<td>" . $row['status'] . "</td>";
 			echo "<td>" . $row['username'] . "</td>";
 			echo "</tr>";
