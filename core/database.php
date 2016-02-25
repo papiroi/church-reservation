2adb3e0d9ba6c2303dc2375110d1abbbaef37153
Michael Adam <madamt0001@gmail.com>
2016-02-26 06:26:24 +0800

    add edit

--- core/database.php
+++ core/database.php
@@ -155,9 +155,21 @@ class Database {
 
 
 	// database/table for user login
-	public function Log() {
+	public function user_log() {
 
-
+		
+		
+	}
+	
+	// function to create reservation log
+	public function reserv_log() {
+		
+		$create_reserv_log = "CREATE TABLE IF NOT EXISTS reserv_log (
+			rLogID int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+			rLogDate date NOT NULL,
+			username varchar(60) NOT NULL,
+			rervationNum varchar(15) NOT NULL
+		)";
 		
 	}
 
